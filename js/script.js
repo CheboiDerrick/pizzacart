a8a17ee3522604f38f1dd0ca7641ce640a21b0cc
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-08-02 19:48:20 +0300

    calculate the total for each pizza item added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function PizzaItem(size, toppings, crust, individualCost) {
     this.cost = individualCost
 }
 
+// get the user inputs
 fetchInputs = () => {
     options = document.querySelectorAll("[name='toppings']:checked");
     size = document.querySelector("[name='sizes']:checked").value;
@@ -17,3 +18,51 @@ fetchInputs = () => {
     console.log(size);
 }
 
+// create new pizza object on every add
+createPizzaItem = () => {
+    fetchInputs();
+    toppings = [];
+    for (let i = 0; i < options.length; i++) {
+        toppings.push(options[i].value);
+    };
+    individualCost = myFunction();
+    function myFunction() {
+        let crustCost
+        let sizeCost
+        let toppingsCost
+        if (size == 'Small') {
+            sizeCost = 100;
+        }
+        else if (size == 'Medium') {
+            sizeCost = 200;
+        }
+        else if (size == 'Large') {
+            sizeCost = 400;
+        }
+        else if (size == 'X-Large') {
+            sizeCost = 600;
+        }
+        console.log(sizeCost);
+        if (crust == 'Crispy') {
+            crustCost = 200;
+        } else if (crust == 'Stuffed') {
+            crustCost = 400;
+        }
+        console.log(crustCost);
+        toppingsCost=0;
+        toppings.forEach(element => {
+            if (element == 'BBQ-Steak') {
+                toppingsCost += 300;
+            }
+            if (element == 'Peri-Peri') {
+                toppingsCost += 300;
+            }
+            if (element == 'Vegan') {
+                toppingsCost += 200;
+            }
+            console.log(toppingsCost);
+        });
+        totalCost=crustCost + sizeCost + toppingsCost
+        console.log(totalCost)
+        return totalCost
+    };
